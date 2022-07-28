2d89968f0c592bf9696bdb1b5bcabd9664c4ba39
Hennessy <khenne222@cable.comcast.com>
2022-07-28 12:40:43 -0400

    added validation and id's

--- script.js
+++ script.js
@@ -11,3 +11,27 @@
 </ul>
 <img src="${}">
 */
+//  First, let’s add validation to notify the user if they forgot to enter a value for any one of the fields. 
+//  Fetch each of the four text input nodes and read their values. If any one is empty, alert the user with window.alert . 
+//  (Hint: you may want to add id attributes to the inputs.)
+// This process is going to look similar to the validation section in the chapter on forms. 
+// Make sure to use preventDefault() to cancel form submission.
+window.addEventListener("load", function () {
+
+   form.addEventListener("submit", (event) => {
+      event.preventDefault();
+   
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargostatus");
+      let itemStatus = document.getElementById("itemStatus");
+      let launchStatus = document.getElementById("launchStatus");
+      let launchStatusCheck = document.getElementById("launchStatusCheck");
+      let missionTarget = document.getElementById("missionTarget");
+      let launchForm = document.getElementById("launchForm");
+
+
+
+
+});
